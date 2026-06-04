87530b34ad1a76b92f3411331acf101635b30e76
norri3 <norri3yt@gmail.com>
2026-06-04 17:04:13 +1000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { LanguageProvider } from "@/components/language-context";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -26,7 +27,9 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className="min-h-screen antialiased font-body">{children}</body>
+      <body className="min-h-screen antialiased font-body">
+        <LanguageProvider>{children}</LanguageProvider>
+      </body>
     </html>
   );
 }
